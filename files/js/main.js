1afc86ede0059e9005842fb1b1436c4c64bcf174
AngusMontgomery <angus.montgomery@digital.cabinet-office.gov.uk>
2017-05-30 15:06:49 +0100

    Added Javascript

--- files/js/main.js
+++ files/js/main.js
@@ -1,1 +1,131 @@
 console.log('it works');
+
+$(document).ready(function() {
+
+$("#current-section").hide();
+$("#interview-section").hide();
+$("#features-section").hide();
+$("#projects-section").hide();
+$("#talks-section").hide();
+$("#films-section").hide();
+
+$("#home-link").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+})
+
+$("#current-link").click(function(){
+	$("#current-section").show();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#interviews-link").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").show();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#features-link").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").show();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#projects-link").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").show();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#talks-link").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").show();
+	$("#films-section").hide();
+});
+
+$("#films-link").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").show();
+});
+
+$("#current-link-1").click(function(){
+	$("#current-section").show();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#interviews-link-1").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").show();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#features-link-1").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").show();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#projects-link-1").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").show();
+	$("#talks-section").hide();
+	$("#films-section").hide();
+});
+
+$("#talks-link-1").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").show();
+	$("#films-section").hide();
+});
+
+$("#films-link-1").click(function(){
+	$("#current-section").hide();
+	$("#interview-section").hide();
+	$("#features-section").hide();
+	$("#projects-section").hide();
+	$("#talks-section").hide();
+	$("#films-section").show();
+});
+
+});
+
+		
